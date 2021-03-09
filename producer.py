40c34d9ded8e603bdcfb78def5f056072ddec0de
fangpf <452639095@qq.com>
2021-03-09 10:07:33 +0800

    调通代码

--- producer.py
+++ producer.py
@@ -18,7 +18,7 @@ from utils.utils import image_process, load_model, process_face_data
 import numpy as np
 
 
-kafka_producer = KafkaProducer(bootstrap_servers='localhost:9092')
+kafka_producer = KafkaProducer(bootstrap_servers='42.193.174.78:9092')
 topic = 'face-detection'
 
 retina_trained_model = './weights/mobilenet0.25_Final.pth'
@@ -47,6 +47,8 @@ def producer():
     video = cv2.VideoCapture(0)
     time.sleep(5)
 
+    count = 1
+
     while video.isOpened():
         success, frame = video.read()
         frame = imutils.resize(frame, width=640, height=640)
@@ -61,8 +63,12 @@ def producer():
             xmin, ymin, xmax, ymax = int(xmin), int(ymin), int(xmax), int(ymax)
             cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 255, 0), 1)
         # cv2.imshow('im', frame)
-        data = cv2.imencode('.jpg', frame)[1].tobytes()
-        kafka_send(topic, data)
+        # data = cv2.imencode('.jpg', frame)[1].tobytes()
+        data = np.asarray(face_result).astype(np.float).tobytes()
+        count += 1
+        if count % 10 == 0:
+            kafka_send(topic, data)
+            count = 1
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         # try:
