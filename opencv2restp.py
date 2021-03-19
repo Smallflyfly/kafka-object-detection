20ad0aad0f40627cda8084eab8e1da743949133c
fangpf <452639095@qq.com>
2021-03-19 10:46:35 +0800

    增加opencv转rstp推流代码

--- opencv2restp.py
+++ opencv2restp.py
@@ -4,22 +4,17 @@
 @author:fangpf
 @time: 2021/02/26
 """
+import sys
 import time
 
 import cv2
 import imutils
 import torch
-from kafka import KafkaProducer
-from kafka.errors import KafkaError
 
 from cfg.config import cfg_mnet
 from models.retinaface import RetinaFace
 from utils.utils import image_process, load_model, process_face_data
-import numpy as np
 
-# bootstrap_servers 可以为公网ip
-kafka_producer = KafkaProducer(bootstrap_servers='localhost:9092')
-topic = 'face-detection'
 
 retina_trained_model = './weights/mobilenet0.25_Final.pth'
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -38,34 +33,32 @@ def detection(im):
     return result_data
 
 
-def kafka_send(topic, data):
-    kafka_producer.send(topic, data)
-
-
 def producer():
 
     video = cv2.VideoCapture(0)
-    time.sleep(5)
-
+    # time.sleep(5)
+    if video.isOpened():
+        print(video.get(cv2.CAP_PROP_FPS))
+        print(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        print(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
     # count = 1
 
     while video.isOpened():
         success, frame = video.read()
-        frame = imutils.resize(frame, width=640, height=640)
-        if not success:
-            break
+        frame = imutils.resize(frame, width=640, height=480)
+        # if not success:
+        #     break
         face_result = detection(frame)
-        if face_result is None:
-            continue
         for det in face_result:
             xmin, ymin, xmax, ymax, conf = det
             # xmin, ymin, xmax, ymax = int(xmin) * 4, int(ymin) * 4, int(xmax) * 4, int(ymax) * 4
             xmin, ymin, xmax, ymax = int(xmin), int(ymin), int(xmax), int(ymax)
             cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 255, 0), 1)
-        # cv2.imshow('im', frame)
-        data = cv2.imencode('.jpg', frame)[1].tobytes()
+        cv2.imshow('im', frame)
+        # data = cv2.imencode('.jpg', frame)[1].tobytes()
+        sys.stdout.buffer.write(frame.tobytes())
         # data = np.asarray(face_result).astype(np.float).tobytes()
-        kafka_send(topic, data)
+        # kafka_send(topic, data)
         # count += 1
         # if count % 10 == 0:
         #     kafka_send(topic, data)
@@ -77,8 +70,8 @@ def producer():
         # except KafkaError as e:
         #     print(e)
         #     break
-
-        print('.', end='', flush=True)
+        # print('.', end='', flush=True)
+        # opencv 转rtsp指令
 
     video.release()
     cv2.destroyAllWindows()
